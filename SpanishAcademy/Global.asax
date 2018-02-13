52aeedb047c7d2edc9313c636f0574ed13b35615
SaranachonI <SaranachonI@SPWSPDITGNB054>
2018-02-13 10:50:22 +0700

    Add project files.

---
+++ SpanishAcademy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpanishAcademy.MvcApplication" Language="C#" %>
---
+++ SpanishAcademy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpanishAcademy.Startup))]
+namespace SpanishAcademy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
