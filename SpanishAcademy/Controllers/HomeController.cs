56d1d7e8f5cf3961ae9ab4f04a49158c2ba35b5c
SaranachonI <SaranachonI@SPWSPDITGNB054>
2018-02-16 18:50:59 +0700

    Updates-codes

--- SpanishAcademy/Controllers/HomeController.cs
+++ SpanishAcademy/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Web.Http;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SpanishAcademy.Controllers
 {
@@ -25,6 +29,16 @@ namespace SpanishAcademy.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public string GetClass()
+        {
+            string json;
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("https://spreadsheets.google.com/feeds/list/1rmG59KueHFkXXIsmXLVh-pOFnTMddk7AyDJjrm1CDGU/od6/public/values?alt=json");
+            }
+            return json;
         }
     }
 }
