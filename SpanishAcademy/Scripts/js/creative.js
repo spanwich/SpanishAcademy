32fb65cc5363a90b97382a9a00f579c7af259073
SaranachonI <SaranachonI@SPWSPDITGNB054>
2018-03-19 17:21:11 +0700

    Update-on-services

--- SpanishAcademy/Scripts/js/creative.js
+++ SpanishAcademy/Scripts/js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
+          scrollTop: (target.offset().top - 50)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 57
+    offset: 100
   });
 
   // Collapse Navbar
